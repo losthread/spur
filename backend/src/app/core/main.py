16f86485edfa368b5c51ea8b59b7475ed6440612
f8tum <parthnaik.office@gmail.com>
2026-06-19 00:14:34 +0530

    feat: add CORS and logging middleware

--- backend/src/app/core/main.py
+++ backend/src/app/core/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from ..routes import users, urls
+from ..crud.logs import log
+from fastapi.middleware.cors import CORSMiddleware
+import logging
 import uvicorn
 
 # create fastAPI instance - app tha uvicorn serves
@@ -8,6 +11,27 @@ app = FastAPI()
 # include routes in app
 app.include_router(users.router)
 app.include_router(urls.router)
+
+# CORS middleware
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['http://localhost:5173'],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
+
+# Custom logging middleware
+@app.middleware("http")
+async def log_ip_middleware(request: Request, call_next):
+  ip = request.client.host
+  try:
+    log(ip)
+
+  except Exception as e:
+    logging.error(f"Failed to log IP: {e}")
+  
+  return await call_next(request)
 
 # run server
 if __name__ == '__main__':
